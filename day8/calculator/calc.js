c223f57a306bfb00c6ff82bd76bcb4fe1beedec8
Ayush-prajapati1 <kumarayush68192@gmail.com>
2025-10-16 12:48:00 +0530

    comit calculator

---
+++ day8/calculator/calc.js
@@ -0,0 +1,57 @@
+const num1=document.getElementById("num1");
+const num2=document.getElementById("num2");
+const op=document.getElementById("output");
+const error=document.getElementById("error");
+
+
+console.log(num1);
+console.log(num2);
+
+function add(){
+
+    if(num1.value==""||num2.value==""){
+        error.innerText='Please enter both number';
+        error.style.color='red';
+        output.innerText="";
+    }else{
+      error.innerText="";
+const op=  parseFloat(num1.value)+parseFloat(num2.value);
+output.innerText=op;
+}
+}
+function sub(){
+     if(num1.value==""||num2.value==""){
+        error.innerText='Please enter both number';
+         error.style.color='red';
+        output.innerText="";
+    }else{
+          error.innerText="";
+   console.log(num1.value);
+console.log(num2.value); 
+const op=  parseFloat(num1.value)-parseFloat(num2.value);
+output.innerText=op;}
+}
+function mul(){
+     if(num1.value==""||num2.value==""){
+        error.innerText='Please enter both number';
+         error.style.color='red';
+        output.innerText="";
+    }else{
+          error.innerText="";
+   console.log(num1.value);
+console.log(num2.value); 
+const op=  parseFloat(num1.value)*parseFloat(num2.value);
+output.innerText=op;}
+}
+function div(){
+     if(num1.value==""||num2.value==""){
+        error.innerText='Please enter both number';
+         error.style.color='red';
+        output.innerText="";
+    }else{
+          error.innerText="";
+   console.log(num1.value);
+console.log(num2.value); 
+const op=  parseFloat(num1.value)/parseFloat(num2.value);
+output.innerText=op;}
+}
